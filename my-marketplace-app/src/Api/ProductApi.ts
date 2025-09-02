aae25291ec8aeb7450114c236f0e5ddf48655385
sheetal sonsale <sheetal.s@ccube.com>
2025-09-02 16:44:36 +0530

    solve api response error

--- my-marketplace-app/src/Api/ProductApi.ts
+++ my-marketplace-app/src/Api/ProductApi.ts
@@ -8,9 +8,11 @@ export interface Product {
   category: string;
   description: string;
 }
-export interface response {
+export interface ApiResponse {
   data: Product[];
   first: number;
+  prev?: number | null;
+  next?: number | null;
   last: number;
   pages: number;
   items: number;
@@ -22,13 +24,13 @@ export const productApi = createApi({
   tagTypes: ["Product"],
   endpoints: (builder) => ({
     getProducts: builder.query<
-      { data: Product[]; total: number }, // Result type
-      { page: number; limit: number } // Arg type
+      { data: Product[]; total: number },
+      { page: number; limit: number }
     >({
       query: ({ page, limit }) => `products?_page=${page}&_per_page=${limit}`,
-      transformResponse: (response: Response) => {
+      transformResponse: (response: ApiResponse) => {
         return {
-          data: response?.data,
+          data: response.data,
           total: response.items,
         };
       },
